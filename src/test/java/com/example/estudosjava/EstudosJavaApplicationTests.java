88c4dc61b0ce881d35f309ab6daedf82b2506519
Alessandra Estephany <alessandraestph@gmail.com>
2025-02-16 14:28:45 -0300

    1° Commit Estudos java

---
+++ src/test/java/com/example/estudosjava/EstudosJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.estudosjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
